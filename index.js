7f67f8930164af838551add24924cd2e3f737749
sanjay <71496141+wolves-coder@users.noreply.github.com>
2025-08-26 13:44:53 +0530

    index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
 
-    if(message.content == 'sup bot') {
+    if(message.content == 'sup') {
         message.reply('sup human');
     }
 });
+
 
 bot.login('NzU2NDYxNTg0NjIwMDYwNzIz.X2SLwA.MGFPL49TuLK_hjVOJHnbjRgDavs');
